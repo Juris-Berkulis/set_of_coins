529d44fb2e75c669b9c00487b9041e1e8ce5e987
Juris-Berkulis <krztfjrbhm1@jmail.com>
2021-10-01 07:58:34 +0300

    1. Add a responsive layout for the site (change the html-code and scss-code); 2. Add minor improvements (change html-code and scss-code); 3. Rename some variables in the js-code.

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,24 +1,24 @@
 function countdownTimeStart(endTime){
 
-    const countDownDate = new Date(endTime).getTime();
+    const finalDate = new Date(endTime).getTime();
     
     const myTimer = setInterval(function() {
     
         const now = new Date().getTime();
         
-        const distance = countDownDate - now;
+        const timeLeft = finalDate - now;
         
-        const days = Math.floor(distance / ( 1000 * 60 * 60 * 24 ));
-        const hours = Math.floor((distance / ( 1000 * 60 * 60 )) % 24 );
-        const minutes = Math.floor((distance / (1000 * 60 )) % 60 );
-        const seconds = Math.floor((distance / 1000 ) % 60 );
+        const days = Math.floor(timeLeft / ( 1000 * 60 * 60 * 24 ));
+        const hours = Math.floor((timeLeft / ( 1000 * 60 * 60 )) % 24 );
+        const minutes = Math.floor((timeLeft / (1000 * 60 )) % 60 );
+        const seconds = Math.floor((timeLeft / 1000 ) % 60 );
         
         document.getElementById("scoreboard__timer_number_days").innerHTML = days;
         document.getElementById("scoreboard__timer_number_hours").innerHTML = hours;
         document.getElementById("scoreboard__timer_number_minutes").innerHTML = minutes;
         document.getElementById("scoreboard__timer_number_seconds").innerHTML = seconds;
         
-        if (distance < 0) {
+        if (timeLeft < 0) {
             clearInterval(myTimer);
             document.getElementById("scoreboard__timer_number_days").innerHTML = '0';
             document.getElementById("scoreboard__timer_number_hours").innerHTML = '0';
